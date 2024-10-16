a2c62169797e962d8ecdc2c2720b11ebde99ad24
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-10-16 17:08:58 +0200

    feat: add delete

--- resources/js/pages/Ura/IndexApartment.vue
+++ resources/js/pages/Ura/IndexApartment.vue
@@ -1,6 +1,7 @@
 <script>
 import { RouterLink } from 'vue-router';
 import Routinglist from './partials/Routinglist.vue';
+import axios from 'axios';
 
 export default {
   name: "IndexApartment",
@@ -11,14 +12,35 @@ export default {
     return {
       name: "",
       apartments: [],
+      message:"",
+      messageType:""
     };
   },
-  methods: {},
+    methods: {
+    deleteApartment(apartment) {
+        if (confirm(`Sei sicuro di voler eliminare l'appartamento "${apartment.title}"?`)) {
+        axios
+            .post('/api/user/utente/dashboard', { data: apartment })  // Simula DELETE usando POST e passando l'oggetto
+            .then((res) => {
+            // Rimuovi l'appartamento dalla lista locale
+            this.apartments = this.apartments.filter(a => a.id !== apartment.id);
+            // Messaggio di successo
+            this.message = `Appartamento "${apartment.title}" eliminato con successo`;
+            this.messageType = 'success';
+            })
+            .catch((err) => {
+            // Messaggio di errore
+            console.error(err);
+            this.message = `Errore durante l'eliminazione dell'appartamento "${apartment.title}"`;
+            this.messageType = 'error';
+            });
+        }
+    }
+    },
   mounted() {
     axios
       .get("/api/user")
       .then((res) => {
-        // console.log(res);
         this.name = res.data.name;
       })
       .catch((err) => {
@@ -28,7 +50,6 @@ export default {
     axios
       .get("api/user/utente/dashboard")
       .then((response) => {
-        console.log(response);
         this.apartments = response.data;
       })
       .catch((err) => {
@@ -47,6 +68,10 @@ export default {
         <div class="row">
             <div class="col-lg-12 col-md-12">
                 <h2 class="mb-3">I miei appartamenti</h2>
+
+                <div v-if="message" :class="['alert', messageType === 'success' ? 'alert-success' : 'alert-danger']">
+                    {{ message }}
+                </div>
 
                 <div class="table-responsive">
                     <table class="table">
@@ -82,7 +107,7 @@ export default {
                                 <a href="#" class="btn btn-warning">
                                 <i class="fa-solid fa-pencil"></i>
                                 </a>
-                                <a href="#" class="btn btn-danger">
+                                <a href="#" class="btn btn-danger" @click.prevent="deleteApartment(apartment)">
                                 <i class="fa-solid fa-trash"></i>
                                 </a>
                             </td>
